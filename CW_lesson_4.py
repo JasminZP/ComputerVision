3cc93c733eb3b13bbdadbd280a48fd19ef6aa12b
Zinchuk Pavlo <150339743+JasminZP@users.noreply.github.com>
2025-10-06 13:52:27 +0300

    Add files via upload

---
+++ CW_lesson_4.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+
+
+img = cv2.imread('images/img3.jpg')
+scale = 1
+img = cv2.resize(img, (int(img.shape[1] * scale), int(img.shape[0] * scale)))
+print(img.shape)
+
+img_copy =  img.copy()
+img_copy_color = img.copy()
+
+
+
+img_copy = cv2.cvtColor(img_copy, cv2.COLOR_BGR2GRAY)
+img_copy = cv2.GaussianBlur(img_copy, (5, 5), 4)
+
+#підсилення контрасту
+img_copy = cv2.equalizeHist(img_copy)
+img_copy = cv2.Canny(img_copy, 100, 250)
+
+#крайній зовнішній контур
+cont, hierarchy = cv2.findContours(img_copy, cv2.RETR_LIST , cv2.CHAIN_APPROX_SIMPLE)
+
+#малювання контурів прямоткунікиів та тексту
+
+for cnt in cont:
+    area = cv2.contourArea(cnt)
+    if area > 90:
+        x, y, w, h = cv2.boundingRect(cnt)
+        cv2.drawContours(img_copy_color, [cnt], -1, (0, 255, 0), 2)
+        cv2.rectangle(img_copy_color, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        text_y = y - 5 if y - 5 > 10 else y + 15
+        text = f'x:{x}, y:{y}, s:{int(area)}'
+        cv2.putText(img_copy_color, text, (x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
+
+cv2.imshow('theblackone', img_copy_color)
+# cv2.imshow('theblackone', img)
+# cv2.imshow('theblackone_copy', img_copy)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
